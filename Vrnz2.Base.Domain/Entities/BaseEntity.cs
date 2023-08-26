f692fe294eadf06f722eb62ecd08a1e678d0df96
Carlos Felippe Vernizze <cfvernizze@gmail.com>
2023-08-26 17:33:44 -0300

    Inlusao da 'BaseEntity'

---
+++ Vrnz2.Base.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Vrnz2.Base.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public bool Deleted { get; set; } = false;
+        public bool Active { get; set; } = true;
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset? LastUpdate { get; set; }
+        public DateTimeOffset? DeletedAt { get; set; }
+    }
+}
